bf49bef091e087e7e0d0939e41246ab3be25960b
bu-luochen <wangzuohaoc@163.com>
2025-10-31 15:48:33 +0800

    adjust9

--- User/main.c
+++ User/main.c
@@ -17,10 +17,10 @@
 
 
 float Target = 0,Actual = 0,Out = 0;
-float Kp = 0.5,Ki = 0.04,Kd = -0.15;
+float Kp = 0.5,Ki = 0.04,Kd = -0.1;
 float Error0 = 0,Error1 = 0,Error2 = 0;
-uint8_t Actual_mode = 0,Target_mode = 0;
-uint8_t mode =0;
+uint8_t Actual_mode = 0;
+uint8_t mode =0,Flag_change = 0;
 int main ()
 {	
 	OLED_Init();
@@ -34,45 +34,42 @@ int main ()
 	
 	while(1)
 	{	
-		if(mode == 0){
 		
-			if(Target_mode == 1){
-				Target = 0;
-				Actual = 0;
-				Target_mode = 0;
-			}
+		if ( Key_GetNum() == 1){
+			mode = (mode + 1) % 2;
+			Motor_SetSpeed(M1,0);
+			Motor_SetSpeed(M2,0);
+			Actual_mode = 0;
+			Target = 0;
+			Actual = 0;
+			Flag_change = 1;
+			
+		}
+		if(mode == 0){
 			if(Serial_RxFlag == 1){
 				Target = 0;
 				if(Serial_RxPacket[0] == '-'){
 					for(int i = 1;Serial_RxPacket[i] != '%';i++){
 						Target = Target * 10 + (Serial_RxPacket[i] - '0');
 					}
-					if(Target >= 100){Target = 100;}
+					//if(Target >= 100){Target = 100;}
 					Target = -Target;
 				} else{  
 					for(int i = 0;Serial_RxPacket[i] != '%';i++){
 						Target = Target * 10 + (Serial_RxPacket[i] - '0');
 					}
-					if(Target >= 100){Target = 100;}
+					//if(Target >= 100){Target = 100;}
 				}
 				
 				Serial_RxFlag = 0;
 				
 			}
 		} else if(mode == 1){
-			
-			if(Target_mode == 0){
-				Target = 0;
-				Actual = 0;
-				Target_mode = 1;
-			}
 			Target += (EI_GetTim3() / 3);
 			
 		}
 		
-		if ( Key_GetNum() == 1){
-			mode = (mode + 1) % 2;
-		}
+		
 		switch(mode){
 			case 0:
 				
@@ -117,13 +114,21 @@ int main ()
 void TIM2_IRQHandler(void)
 {
 	static uint16_t Count;
-	static int16_t Speed;
 	if(TIM_GetITStatus(TIM2,TIM_IT_Update)==SET){
 		Serial_Tick();
 		Key_Tick();
 		Count ++;
 		if(Count >= 20){
 			Count = 0;
+			
+			if(Flag_change == 1){
+				Motor_SetSpeed(M1,0);
+				Motor_SetSpeed(M2,0);
+				Actual_mode = 0;
+				Target = 0;
+				Actual = 0;
+				Flag_change = 0;
+			}
 			
 			Error2 = Error1;
 			Error1 = Error0;
